7a32933a4f47e576cd857743500661b7bc4871b4
Rodrigo Guimarães <rodrigodeoliveira@hotmail.com.br>
2018-07-01 23:40:52 -0300

    rm linhas que estragam banco

--- 4.Sistema/tradebitcoin/config/environment.rb
+++ 4.Sistema/tradebitcoin/config/environment.rb
@@ -3,8 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-ActiveRecord::Base.establish_connection(
-    :adapter  => 'sqlite3',
-    :timeout  => 1000
-)
